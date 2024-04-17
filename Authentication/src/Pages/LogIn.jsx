1c47c88eb007ec7b719de944d04af8532a99e466
Muhammad Hammad <hammadkhalil04@gmail.com>
2024-04-17 08:34:59 -0700

    Feat:"2 Screens Added"

--- Authentication/src/Pages/LogIn.jsx
+++ Authentication/src/Pages/LogIn.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+
 function LogIn() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
@@ -16,42 +17,55 @@ function LogIn() {
   }
 
   return (
-    <form className="SignUpPage">
-      <h1>Login Here</h1>
-      <p>Welcome back! please enter your detail</p>
-      <br />
-      <label for="username">UserName: </label>
-      <input
-        type="username"
-        placeholder="Enter UserName"
-        value={email}
-        required
-        onChange={getEmail}
-      />
-      <br />
-      <br />
+    <div className="flex">
+      <div className="w-1/2 h-screen bg-[#4BCBEB]">
+        <h1 className="text-center">Task List Manger</h1>
+      </div>
+      <div className="w-1/2">
+        <div className="flex items-center justify-center ">
+          <form >
+            <h1 className="text text-center text-lg font-bold mt-44">Login Here</h1>
+            <p className="text-xs text-center mt-2">Welcome back! please enter your detail</p>
+            <br />
+            
+            <input
+              className="mt-0.1 w-64 px-3 py-2 rounded border border-slate-300"
+              type="username"
+              placeholder="Enter UserName"
+              value={email}
+              required
+              onChange={getEmail}
+            />
+            <br />
+            <br />
 
-      <label for="password">Password: </label>
-      <input
-        type="password"
-        placeholder="Enter Password"
-        value={password}
-        required
-        onChange={getPassword}
-      />
-      <br />
-      <br />
+            
+            <input
+              className="mt-0.1 w-64 px-3 py-2 rounded border border-slate-300"
+              type="password"
+              placeholder="Enter Password"
+              value={password}
+              required
+              onChange={getPassword}
+            />
+            <br />
+            <br />
 
-      <input type="checkbox" for="checkbox" />
-      <label for="checkbox">Remember me </label>
-      <Link to={"/Forget"}>
-        <text>Forget Password?</text>
-      </Link>
+            <input type="checkbox" for="checkbox" />
+            <text className="text-xs mx-0.5"for="checkbox">Remember me </text>
+            <Link to={"/Forget"}>
+              <button className="text-blue-800 text-xs mx-10">Forget Password?</button>
+            </Link>
 
-      <br />
-      <br />
-      <button>Login</button>
-    </form>
+            <br />
+            <br />
+            <button className="bg-[#4BCBEB] px-2 py-2 w-64 rounded-2xl">Login</button><br/><br/>
+
+            <Link to='/'  ><text className="px-6 text-xs ">Don't have an Account?</text><button className="text-blue-800 text-xs">SignUp</button> </Link>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- Authentication/src/Pages/SignUp.jsx
+++ Authentication/src/Pages/SignUp.jsx
@@ -30,25 +30,41 @@ function SignUp(){
     }
    
     return (
-        <form className='SignUpPage'>
-      <h1>Register Here</h1>
-      <br/>
-      <label for='name'>Name: </label>
-      <input type='name' placeholder='Enter Name'  onChange={getName} required/><br/><br/>
-      
-      <label for='email'>Email: </label>
-      <input type='email' placeholder='Enter Email'  onChange={getEmail} required/><br/><br/>
-      <label for='password'>Password: </label>
-      <input id='password' placeholder='Enter Password'  onChange={getPassword} onClick={togglePassword} required/>
-      
-      <br/><br/>
+        
+      <div className="flex h-screen">
+        <div className="w-1/2 h-screen bg-[#4BCBEB]">
+         <h1 className="text-center">Task List Manager</h1>
+        </div>
+        <div className="w-1/2  ">
+          <h1 className="text text-center text-lg font-bold mt-44">Sign Up for an Account</h1>
 
-      <input type="checkbox" for='checkbox'/>
-      <label for='checkbox'>By creating an account means you agree <br/>to the Terms & conditions and our Privacy Policy</label>
-      <br/><br/>
-      <Link to='/Login'><button >Sign Up</button></Link>
-      <Link to='/Login'><button >Already have Account</button> </Link>
-    </form>
+          <div className="flex items-center justify-center ">
+         
+            <form >
+              
+              <br/>
+              
+              <input className="mt-0.1 w-60 px-3 py-2 rounded border border-slate-300" type='name' placeholder='Enter Name'  onChange={getName} required/><br/><br/>
+              
+              
+              <input className='mt-0.1 w-60 px-3  py-2 rounded border border-slate-300' placeholder='Enter Email'  onChange={getEmail} required/><br/><br/>
+              
+              <input className='mt-0.1 w-60 px-3  py-2 rounded border border-slate-300' placeholder='Enter Password'  onChange={getPassword} onClick={togglePassword} required/>
+
+              <p className="text-xs">Password must be 8 characters</p>
+              
+              <br/><br/>
+
+              <input type="checkbox" for='checkbox'/>
+              <label for='checkbox'>Terms & Conditions</label>
+              <br/><br/>
+              <Link to='/Login'><button className="bg-[#4BCBEB] px-2 py-2 w-60 rounded-2xl">Sign Up</button></Link><br/><br/>
+             
+              <Link to='/Signup' ><text >Already have an Account?</text><button className="text-blue-800">Login</button> </Link>
+          </form>
+        </div>
+      </div>
+    </div>
     )
 }
 
