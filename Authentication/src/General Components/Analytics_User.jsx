b44d80f8280bd814a51b315f0f9f5a7a56cacaa5
Muhammad Hammad <hammadkhalil04@.com>
2024-04-22 00:47:44 +0500

    feat:"Modal Added"

--- Authentication/src/General Components/Analytics_User.jsx
+++ Authentication/src/General Components/Analytics_User.jsx
@@ -1,48 +1,46 @@
 import React from "react";
 import Chart from "../General Components/Chart";
-import Calendar from '../General Components/Calendar'
-
+import Calendar from "../General Components/Calendar";
 
 function Analytics_User() {
-    return (
-        
-        
-        <div className=" bg-gray-300 w-auto h-[93.6%] border ">
-            <div className=" bg-white border-1 h-[92%] mt-8 py-12 mx-16 mb-4 ">
-                <div className="grid grid-cols-4 space-x-2 px-12 py-12 ">
-                    <div className="h-32  rounded-md  border-1 bg-pink-100">
-                        <p className="mx-8">Total Task</p>
-                        <p className="text-xl mt-2 px-4 text-gray-600">90/100</p>
-                    </div>
-                    <div className="h-32 rounded-md  border-1 bg-sky-200">
-                        <p className="mx-8">Completed Task</p>
-                        <p className="text-xl px-4 mt-2 text-gray-600">80/100</p>
-                    </div>
-                    <div className="h-32  rounded-md   border-1 bg-orange-100">
-                        <p className="mx-8">Pending Task</p>
-                        <p className="text-xl px-4 mt-2 text-gray-600">50/100</p>
-                    </div>
-                    <div className="h-32 rounded-md  border-1 bg-emerald-100">
-                        <p className="mx-8">Decline Task</p>
-                        <p className="text-xl px-4 mt-2 text-gray-600">10/100</p>
-                    </div>
-
-                </div>
-                <div className="grid grid-cols-2 space-x-4 mb-32 px-4 mt-4 bg-white">
-                    <div className="h-52 rounded-md   border-1 bg-white">
-                        <p className="mx-40 font-bold  ">Total Task Ratio</p>
-                        <div className="px-8"> <Chart ></Chart></div> 
-                    </div>
-                    <div className="h-52 rounded-md   border-1 bg-white">
-                        <div className="px-24"><Calendar></Calendar></div>
-                         
-                    </div>
-                </div>
+  return (
+    <div className=" bg-gray-300 w-auto h-[93.6%] border ">
+      <div className=" bg-white border-1 h-[92%] mt-8 py-12 mx-16 mb-4 ">
+        <div className="grid grid-cols-4  ">
+          <div className="h-32  rounded-md  border-1 bg-pink-100">
+            <p className="mx-8">Total Task</p>
+            <p className="text-xl mt-2 px-4 text-gray-600">90/100</p>
+          </div>
+          <div className="h-32 rounded-md  border-1 bg-sky-200">
+            <p className="mx-8">Completed Task</p>
+            <p className="text-xl px-4 mt-2 text-gray-600">80/100</p>
+          </div>
+          <div className="h-32  rounded-md   border-1 bg-orange-100">
+            <p className="mx-8">Pending Task</p>
+            <p className="text-xl px-4 mt-2 text-gray-600">50/100</p>
+          </div>
+          <div className="h-32 rounded-md  border-1 bg-emerald-100">
+            <p className="mx-8">Decline Task</p>
+            <p className="text-xl px-4 mt-2 text-gray-600">10/100</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 space-x-4 mb-32 px-4 mt-4 bg-white">
+          <div className="h-52 rounded-md   border-1 bg-white">
+            <p className="mx-40 font-bold  ">Total Task Ratio</p>
+            <div className="px-8">
+              {" "}
+              <Chart></Chart>
             </div>
-        </div>    
-        
-     
-    );
-  }
-  
+          </div>
+          <div className="h-52 rounded-md   border-1 bg-white">
+            <div className="px-24">
+              <Calendar></Calendar>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default Analytics_User;
--- Authentication/src/Tasks/Task.jsx
+++ Authentication/src/Tasks/Task.jsx
@@ -1,17 +1,93 @@
-import React from "react";
+import { useState, React } from "react";
 import Header from "../General Components/Header";
 import MenuComponent from "../General Components/MenuComponent";
+import AddModal from "../General Components/AddModal";
 
 function Task() {
+  const [submittedData, setSubmittedData] = useState([]);
+  let [showModal, setShowModal] = useState(false);
+
+  function display() {
+    setShowModal(!showModal);
+  }
+
+  function handleModalSubmit(data) {
+    setSubmittedData([...submittedData, data]);
+    setShowModal(false);
+  }
+
+  function getRandomColor() {
+    const colors = ["red", "yellow", "orange", "pink", "purple"];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  }
+
   return (
     <div>
-      <Header pageName="Tasks"></Header>
+      <Header pageName="Task"></Header>
       <div className="flex h-auto">
         <div className="flex h-screen w-[16.6%] ">
           <MenuComponent color3={"blue-700"}></MenuComponent>
         </div>
         <div className="w-[84.4%]  border-1   bg-[gray]">
-          <div className=" h-screen w-[82%] mx-auto mt-12 mb-12 bg-white"></div>
+          <div className="flex mt-12 ml-8">
+            <h1 className="font-bold text-xl ">Start date:</h1>
+            <h2 className="font-bold ml-52 text-xl">End date:</h2>
+
+            <button className="mx-auto" onClick={display}>
+              <img className="ml-96" src="src\assets\Add Task.png"></img>
+            </button>
+          </div>
+          <div className="flex  ">
+            <input
+              className="ml-8 w-[19%] h-12 mt-4 rounded"
+              type="date"
+              placeholder="15-Apr-2024"
+              required
+            ></input>
+            <input
+              className="ml-16 w-[20%] h-12 mt-4 rounded"
+              type="date"
+              placeholder="15-Apr-2024"
+              required
+            ></input>
+          </div>
+          <h3 className="font-bold ml-8 mt-8 text-xl">Enter Title:</h3>
+          <div className="flex border-1 border-blue-700">
+            <input
+              className=" rounded rounded-l-md rounded-r-none ml-8 w-[36%] h-12 mt-4 "
+              type="search"
+              placeholder="Search"
+              required
+            ></input>
+            {/* <img className="absoulte left-2 " src="src\assets\Search.png"></img> */}
+            <button className=" bg-blue-200 w-[8%] rounded-l-none rounded-r-md h-12 mt-4">
+              Search
+            </button>
+          </div>
+          <div className="grid grid-cols-3 space-x-12 px-8 mt-4 border-1 ">
+            {submittedData.map((data, index) => (
+              <div key={index} className="h-64 border-2 border-black rounded">
+                <div
+                  className={`p-4 bg-${getRandomColor()}-500 overlay border-2 border-black`}
+                ></div>
+                <h4 className="font-bold px-2">Title:</h4>
+                <p className="px-2">{data.title}</p>
+                <h5 className="font-bold px-2">Description:</h5>
+                <p className="px-2"> {data.description}</p>
+                <h6 className="font-bold px-2">Attachment</h6>
+                <div className="flex">
+                  <h7 className="font-bold px-2">Start Date:</h7>
+                  <h8 className="font-bold mx-auto">End Date:</h8>
+                </div>
+                <div className="flex">
+                  <p>{data.startDate}</p>
+                  <p>{data.endDate}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>{showModal && <AddModal onSubmit={handleModalSubmit} />}</div>
         </div>
       </div>
     </div>
