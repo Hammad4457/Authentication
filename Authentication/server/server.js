48e5bea9f4eecee757a36550d00a8911131d5b0c
Muhammad Hammad <hammadkhalil04@gmail.com>
2024-05-01 08:14:59 -0700

    Feat:"Backend Skeleton"

--- Authentication/server/server.js
+++ Authentication/server/server.js
@@ -1,10 +1,26 @@
-const express = require("express");
+import mongoose from "mongoose";
+import express from 'express'
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree"] });
-});
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
-});
+
+app.get("/",(req,res)=>{
+  res.send("asdsada");
+})
+
+
+
+const start = async()=>{
+  try{
+    await mongoose.connect("mongodb+srv://hammadkhalil04:N6w3EPoTi5k0ddHp@cluster0.g08qzv6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    app.listen(3001, () => {
+      console.log("Server started on port 3001");
+    });
+
+  }
+  catch(e){
+    console.log(e);
+  }
+} 
+
+start();
